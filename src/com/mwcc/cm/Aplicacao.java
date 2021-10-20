c7c885c5170492fc489d7a0625a234ec3155ca0a
Marcio Willian <willian_mw.cc@hotmail.com>
2021-10-19 23:46:07 -0300

    Implementando o Tabuleiro e exception de saída

--- src/com/mwcc/cm/Aplicacao.java
+++ src/com/mwcc/cm/Aplicacao.java
@@ -1,16 +1,14 @@
 package com.mwcc.cm;
 
 import com.mwcc.cm.modelo.Tabuleiro;
+import com.mwcc.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alterarMarcacao(4, 4);
-		tabuleiro.alterarMarcacao(4, 5);
-				
-		System.out.println(tabuleiro.toString());
+		new TabuleiroConsole(tabuleiro);
+
 	}
 }
  
---
+++ src/com/mwcc/cm/excecao/SairException.java
@@ -0,0 +1,6 @@
+package com.mwcc.cm.excecao;
+
+public class SairException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+}
