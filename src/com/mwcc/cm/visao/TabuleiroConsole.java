3b98c5108916083d6928fe0cc3c0094ff668011d
Marcio Willian <willian_mw.cc@hotmail.com>
2021-10-23 21:32:49 -0300

    D - Concluindo o projeto do campo minado

--- src/com/mwcc/cm/visao/TabuleiroConsole.java
+++ src/com/mwcc/cm/visao/TabuleiroConsole.java
@@ -45,35 +45,37 @@ public class TabuleiroConsole {
 		try {
 			while (!tabuleiro.objetivoAlcancado()) {
 				System.out.println(tabuleiro);
-				
+
 				String digitado = capturarValorDigitado("Digite (x, y): ");
-				
-				Iterator<Integer> xy = Arrays.stream(digitado.split(","))
-				.map(e -> Integer.parseInt(e.trim())).iterator();
-				
+
+				Iterator<Integer> xy = Arrays.stream(digitado.split(",")).map(e -> Integer.parseInt(e.trim()))
+						.iterator();
+
 				digitado = capturarValorDigitado("1 - Abrir ou 2 - (Des)Marcar: ");
-				
-				if("1".equals(digitado)) {
+
+				if ("1".equals(digitado)) {
 					tabuleiro.abrir(xy.next(), xy.next());
-				}else if("2".equals(digitado)){
+				} else if ("2".equals(digitado)) {
 					tabuleiro.alterarMarcacao(xy.next(), xy.next());
 				}
 			}
-			
+
+			System.out.println(tabuleiro);
 			System.out.println("Você ganhou!!!!");
 		} catch (ExplosaoException e) {
+			System.out.println(tabuleiro);
 			System.out.println("Você perdeu!");
 		}
 	}
-	
+
 	private String capturarValorDigitado(String texto) {
 		System.out.print(texto);
 		String digitado = entrada.nextLine();
-		
-		if("sair".equalsIgnoreCase(digitado)) {
+
+		if ("sair".equalsIgnoreCase(digitado)) {
 			throw new SairException();
 		}
-		
+
 		return digitado;
 	}
 }
